affbaedfab52e0eaea47619d6057955be9b2de8f
Galilea-Ivy <ggarcia87@ivytech.edu>
2022-04-11 19:32:44 -0400

    added convertion functions

---
+++ convert.js
@@ -0,0 +1,18 @@
+// Global Variables
+
+
+// Functions
+
+function convertCtoF(cel) {
+    let fahren = cel * (9/5) + 32;
+    return fahren;
+}
+
+function convertFtoC(fahren) {
+    let cel = (fahren - 32) * (5/9);
+    return cel;
+}
+
+function domLoaded(event) {
+
+}
